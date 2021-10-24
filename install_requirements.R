3128cbd12dd8c7df91870c421320c9b839a86480
Fred C <34587882+fdclavel@users.noreply.github.com>
2021-10-24 01:22:18 -0700

    minor user selection feature additions

--- install_requirements.R
+++ install_requirements.R
@@ -4,8 +4,16 @@ pkgs <- as.character(installed.packages()[,c(1)])
 install_pkgs <- pkgs[!(pkgs %in% reqs)]
 
 if(length(install_pkgs) > 0) {
-  install.packages(install_pkgs)
-} else {
+  message("ALERT: ", length(install_pkgs), " new packages ready to install.")
+  message("ALERT: Installing multiple packages can take several minutes.")
+  yn = readline(prompt="PROCEED WITH INSTALLATION? Please select [y/n]: ")
+  if(yn == "y" | yn == "Y") {
+    install.packages(install_pkgs)
+  } else {
+      message("ALERT: Process terminated by user or invalid command received.")
+    }
+  
+  } else {
   message("PROCESS COMPLETE. PACKAGES INSTALLED: 0")
   message("INSTALLATION ABORTED - ALL PACKAGE REQUIREMENTS ALREADY SATISFIED")
 }
